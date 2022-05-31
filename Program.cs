41cb18e9113b6fdff9ad35633464554d723cb7d9
YayoReynoso <eduardo_3reynoso@hotmail.com>
2022-05-31 11:12:20 -0700

    Cambios

--- Program.cs
+++ Program.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace EscrituraArchivos
 {
     class Program
     {
+        //Reynoso Rosales Eduardo Guadalupe #21212036
         static void Main(string[] args)
         {
+            StreamWriter sw = new StreamWriter("ejemplo.txt");
+
+            //Si no existe el archivo, se creará.
+            //Si existe, solo escribirá en él
+
+            string[] lines =
+            {
+                "Esta es la informacion de la primera linea",
+                "Segunda linea",
+                "Tercera linea",
+                "Fin del texto"
+
+            };
+
+            foreach(string line in lines)
+            {
+                sw.WriteLine(line);
+            }
+            sw.Close(); //Se debe cerrar el archivo
+
+            Console.WriteLine("Se escribió en el archivo...");
+            Console.ReadKey();
         }
     }
 }
